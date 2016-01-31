04bf087d5099c2551b94ba38ae12045aa7ee62f1
Desmond Bowe <desmond@crevalle.io>
2016-01-30 20:05:10 -0500

    authenticate and robustify requests

--- lib/beatr.rb
+++ lib/beatr.rb
@@ -2,6 +2,7 @@ require 'beatr/version'
 
 module Beatr
   require 'net/http'
+  require 'json'
 
   def self.configure
     yield config
@@ -12,13 +13,21 @@ module Beatr
     @@config ||= Config.new
   end
 
-  def self.beat name = config.name
+  def self.beat name
     raise ArgumentError, 'please supply a beat name' if name.nil? || name == ''
+    raise 'Please configure with an api key' if config.api_key.nil?
 
-    uri = URI.parse "#{config.host}#{config.endpoint}/#{URI.escape name}"
+    uri = URI.parse "#{config.host}#{config.endpoint}"
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    request = Net::HTTP::Post.new uri.request_uri
+    request.body = { 'beat' => name }.to_json
+
+    request['Content-Type'] = 'application/json'
+    request['X-API-KEY' ] = config.api_key
 
     Thread.new do
-      Net::HTTP.post_form uri, {}
+      http.request request
     end
 
     true
@@ -27,6 +36,7 @@ module Beatr
   class Config
 
     attr_accessor :name
+    attr_accessor :api_key
     attr_reader :host
 
     def initialize
